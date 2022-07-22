be3adc99f7a70cdfaf7509ec745c32e33c2a9195
JaiRaga <44367062+JaiRaga@users.noreply.github.com>
2022-07-22 09:32:30 +0530

    :sparkles: added logic for rating

--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -4,58 +4,60 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 import styles from './styles';
 
-const ProductItem = () => {
+interface ProductItemProps {
+  item: {
+    id: string;
+    title: string;
+    image: string;
+    avgRating: number;
+    ratings: number;
+    price: number;
+    oldPrice?: number;
+  };
+}
+
+const ProductItem = ({item}: ProductItemProps) => {
   return (
     <View style={styles.root}>
       <View style={styles.innerContainer}>
         {/* left container */}
         <Image
           source={{
-            uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/products/cleanarchitecture.jpg',
+            uri: item.image,
           }}
           style={styles.image}
         />
         {/* right container */}
         <View style={styles.rightContainer}>
           <Text style={styles.title} numberOfLines={3}>
-            Logitech MX Master 3 Advanced Wireless Mouse for Mac - Bluetooth/USB
+            {item.title}
           </Text>
           {/* Ratings */}
           <View style={styles.ratingsContainer}>
-            <FontAwesome
-              style={styles.star}
-              name="star"
-              size={18}
-              color="#e47911"
-            />
-            <FontAwesome
-              style={styles.star}
-              name="star"
-              size={18}
-              color="#e47911"
-            />
+            {[0, 0, 0, 0, 0].map((ele, ind) => (
+              <FontAwesome
+                key={`${item.id}-${ind}`}
+                style={styles.star}
+                name={ind < Math.floor(item.avgRating) ? 'star' : 'star-o'}
+                size={18}
+                color="#e47911"
+              />
+            ))}
+            {/*
             <FontAwesome
               style={styles.star}
               name="star-half-full"
               size={18}
               color="#e47911"
             />
-            <FontAwesome
-              style={styles.star}
-              name="star-o"
-              size={18}
-              color="#e47911"
-            />
-            <FontAwesome
-              style={styles.star}
-              name="star-o"
-              size={18}
-              color="#e47911"
-            />
-            <Text style={styles.ratingsCount}>20,000</Text>
+          */}
+            <Text style={styles.ratingsCount}>{item.ratings}</Text>
           </View>
           <Text style={styles.price}>
-            from $17.99 <Text style={styles.oldPrice}>$19.99</Text>
+            from ${item.price}{' '}
+            {item.oldPrice && (
+              <Text style={styles.oldPrice}>${item.oldPrice}</Text>
+            )}
           </Text>
         </View>
       </View>

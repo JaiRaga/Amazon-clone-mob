9c46c76269dc8306cb07c98d7eedf2c95f6b4cf2
JaiRaga <44367062+JaiRaga@users.noreply.github.com>
2022-07-24 17:15:51 +0530

    :sparkles: added logic to prevent item quantity to have negative value

--- src/components/QuantitySelector/QuantitySelector.tsx
+++ src/components/QuantitySelector/QuantitySelector.tsx
@@ -1,24 +1,32 @@
-import { View, Text, Pressable } from 'react-native'
-import React from 'react'
-import styles from './styles'
+import {View, Text, Pressable} from 'react-native';
+import React from 'react';
+import styles from './styles';
 
-const QuantitySelector = () => {
+interface QuantitySelectorProps {
+  quantity: number;
+  setQuantity: React.Dispatch<React.SetStateAction<number>>;
+}
 
-  const onMinus = () => {}
+const QuantitySelector = ({quantity, setQuantity}: QuantitySelectorProps) => {
+  const onMinus = () => {
+    setQuantity(Math.max(0, quantity - 1));
+  };
 
-  const onPlus = () => {}
+  const onPlus = () => {
+    setQuantity(quantity + 1);
+  };
 
   return (
     <View style={styles.root}>
       <Pressable onPress={onMinus} style={styles.button}>
         <Text style={styles.buttonText}>-</Text>
       </Pressable>
-      <Text style={styles.quantity}>0</Text>
+      <Text style={styles.quantity}>{quantity}</Text>
       <Pressable onPress={onPlus} style={styles.button}>
         <Text style={styles.buttonText}>+</Text>
       </Pressable>
     </View>
-  )
-}
+  );
+};
 
-export default QuantitySelector
+export default QuantitySelector;

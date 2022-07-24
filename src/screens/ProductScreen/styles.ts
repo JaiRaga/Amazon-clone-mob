245ba6c04c45532c6d0e81dd50899269fa48cf97
JaiRaga <44367062+JaiRaga@users.noreply.github.com>
2022-07-24 17:18:35 +0530

    :art: added styling

--- src/screens/ProductScreen/styles.ts
+++ src/screens/ProductScreen/styles.ts
@@ -1,15 +1,22 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
+  root: {
+    padding: 10,
+    backgroundColor: 'white'
+  },
   title: {
     color: 'black'
   },
   description: {
-    color: 'black'
+    color: 'black',
+    marginVertical: 10
   },
   price: {
     fontSize: 18,
+    color: 'black',
     fontWeight: 'bold'
+
   },
   oldPrice: {
     color: 'black',
